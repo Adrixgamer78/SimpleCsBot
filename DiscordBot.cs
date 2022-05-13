9720b7389b45af0190362c022ec9f6a58e3a5ce7
Raiden Shogun <96011415+Almighty-Shogun@users.noreply.github.com>
2022-05-13 13:45:22 +0200

    Add files via upload

---
+++ DiscordBot.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.WebSocket;
+using SimpleCsBot.Modules.Events;
+
+namespace SimpleCsBot;
+
+public class DiscordBot
+{
+    private readonly DiscordSocketClient _client;
+
+    private readonly Ready _ready;
+    private readonly MessageReceived _messageReceived;
+    private readonly InteractionCreated _interactionCreated;
+
+    public static DateTime BotStarted;
+
+    public DiscordBot(DiscordSocketClient client, Ready ready, MessageReceived messageReceived, InteractionCreated interactionCreated)
+    {
+        _client = client;
+        _ready = ready;
+        _messageReceived = messageReceived;
+        _interactionCreated = interactionCreated;
+    }
+
+    public async Task RunAsync()
+    {
+        await _client.LoginAsync(TokenType.Bot, "");
+        await _client.SetGameAsync("Genshin Impact");
+        await _client.SetStatusAsync(UserStatus.Idle);
+        await _client.StartAsync();
+
+        _client.Ready += _ready.HandleEventAsync;
+        _client.InteractionCreated += _interactionCreated.HandleEventAsync;
+        _client.MessageReceived += _messageReceived.HandleEventAsync;
+
+        await Task.Delay(Timeout.Infinite);
+    }
+    
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# SimpleCsBot
+Simple base for Discord bots using Discord.NET
+
+- Works with interactions (slash commands, modals, buttons, etc..)
+- Works with text commands (prefix)
+
+### To-do:
+- `DiscordBot.cs` at line 27 put ur bot token there.
+- `Ready.cs` at line 30 remove `101010101010` and put ur guild ID there.
